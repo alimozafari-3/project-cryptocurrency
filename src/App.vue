f084c4f8f39c95863b9562b20aba91fc08c641d4
ali <ali.mozafari733@yahoo.com>
2023-07-08 23:30:03 +0430

    add container and data getdatamarket

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,35 @@
 <template>
- 
+  <div id="app" class="container">
+     
+    <div class="input-group flex-nowrap mb-5 mt-5 shadow-lg bg-body rounded">
+      <input
+        type="text"
+        class="form-control border-info"
+        placeholder="search"
+       v-model="search"
+      />
+    </div>
+
+    <table class="table table-dark table-striped">
+      <thead>
+        <tr>
+          <th scope="col"># name</th>
+          <th scope="col">price</th>
+          <th scope="col">changepercentage</th>
+          <th scope="col">marketcap</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-for="data in Getdatamarket" :key="data">
+          <th scope="row">{{data.market_cap_rank}} <img :src="data.image" alt="" width="23" /> {{ data.name }}</th>
+          <td class="text">${{ data.current_price }}</td>
+          <td class="text">{{ data.market_cap_change_percentage_24h }}%</td>
+          <td class="text">{{ data.market_cap }}</td>
+        </tr>
+      </tbody>
+    </table>
+  
+  </div>
 </template>
 
 <script>
